7b2c1da41bf18eb89f70909d3ea36cf9161a012c
acl587 <albert.lansang@gmail.com>
2013-11-04 19:18:00 -0600

    Added marker plots on map

--- src/cs371/letitfly/MapsActivity.java
+++ src/cs371/letitfly/MapsActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -25,7 +26,7 @@ public class MapsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         Bundle b = getIntent().getExtras();
- 
+     
         try {
             // Loading map
             initilizeMap(b);
@@ -34,6 +35,35 @@ public class MapsActivity extends Activity {
             e.printStackTrace();
         }
  
+    }
+    
+    public Double getCoord(String value){
+    	Bundle b = getIntent().getExtras();
+    	
+    	double dist = .006/6371; //hardcoded km
+    	double brng = Math.toRadians(b.getDouble("azimuth"));
+    	double lat1 = Math.toRadians(b.getDouble("latitude"));
+    	double lon1 = Math.toRadians(b.getDouble("longitude"));
+
+    	double latitude = Math.asin( Math.sin(lat1)*Math.cos(dist) + Math.cos(lat1)*Math.sin(dist)*Math.cos(brng) );
+    	double a = Math.atan2(Math.sin(brng)*Math.sin(dist)*Math.cos(lat1), Math.cos(dist)-Math.sin(lat1)*Math.sin(latitude));
+    	System.out.println("a = " +  a);
+    	double lon2 = lon1 + a;
+
+    	double longitude = (lon2+ 3*Math.PI) % (2*Math.PI) - Math.PI;
+    	
+    	
+    	if(value.equals("long")){
+    		Toast.makeText(getApplicationContext(), 
+        			"Longitude = "+Math.toDegrees(longitude), Toast.LENGTH_LONG).show();
+    		return Math.toDegrees(longitude);
+    	}
+    	else if(value.equals("lat")){
+    		Toast.makeText(getApplicationContext(), 
+        			"Latitude = "+Math.toDegrees(latitude), Toast.LENGTH_LONG).show();
+    		return Math.toDegrees(latitude);
+    	}
+    	return null;
     }
     
     @Override
@@ -68,16 +98,22 @@ public class MapsActivity extends Activity {
          // latitude and longitude
             double latitude = b.getDouble("latitude");
             double longitude = b.getDouble("longitude");
+            double latDest = getCoord("lat");
+            double longDest = getCoord("long");
             CameraPosition cameraPosition = new CameraPosition.Builder().target(
-                    new LatLng(latitude, longitude)).zoom(15).build();
+                    new LatLng(latDest, longDest)).zoom(20).build();
      
             googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
            
             // create marker
             MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Object Thrown From Here");
-             
+            MarkerOptions markerDest = new MarkerOptions().position(new LatLng(latDest, longDest)).title("Object Landed Here").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+            
+            
+            
             // adding marker
             googleMap.addMarker(marker);
+            googleMap.addMarker(markerDest);
  
             // check if map is created successfully or not
             if (googleMap == null) {
