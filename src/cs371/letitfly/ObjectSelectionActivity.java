523cdb51433ba04cca0b241964049a09e013b575
acl587 <albert.lansang@gmail.com>
2013-11-05 17:49:35 -0600

    Created add/delete object functionality
    On first start up, app populates list with file projectiles.txt in
    /assets/. Then uses shared preferences to manage map of projectiles.
    Long press to delete object.

--- src/cs371/letitfly/ObjectSelectionActivity.java
+++ src/cs371/letitfly/ObjectSelectionActivity.java
@@ -1,42 +1,218 @@
 package cs371.letitfly;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import cs371.letitfly.physics.Projectile;
 
 public class ObjectSelectionActivity extends Activity {
 
 	private ListView listView;
+	private double objectMassSelected;
+	ArrayList<String> object_array_list = new ArrayList<String>();
+	private HashMap<String, Double> map = new HashMap<String, Double>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_obj);
+		
+		//toast.makeText(getApplicationContext(), "create", //toast.LENGTH_SHORT).show();
+		
+		Bundle bundle = this.getIntent().getExtras();
+		if(bundle!=null)
+		{
+			map.put(bundle.getString("newObject"), bundle.getDouble("newWeight"));
+		}
+		
+		
+		Map<String,?> keys =  getSharedPreferences("projectilePrefs", 0).getAll();
+		if(keys.isEmpty()==false)
+		{
+			//toast.makeText(getApplicationContext(), "shared prefs", //toast.LENGTH_SHORT).show();
+			getSharedPrefs();
+		}
+		else
+		{
+			//toast.makeText(getApplicationContext(), "assets pop", //toast.LENGTH_SHORT).show();
+			populateWithAssets();
+		}
+		
+		
+		//populateListView();
+
 		listView = (ListView) findViewById(R.id.object_list);
-		ArrayAdapter<Projectile> arrayAdapter =      
-				new ArrayAdapter<Projectile>(this, android.R.layout.simple_list_item_1, Projectile.values());
+		ArrayAdapter<String> arrayAdapter =      
+				new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, object_array_list);
 		listView.setAdapter(arrayAdapter); 
+		listView.setLongClickable(true);
 		listView.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
-				Projectile selectedFromList = (Projectile) (listView.getItemAtPosition(myItemInt));
-				//Toast.makeText(getApplicationContext(), selectedFromList, Toast.LENGTH_SHORT).show();
+				String selectedFromList = (String) (listView.getItemAtPosition(myItemInt));
+				////toast.makeText(getApplicationContext(), selectedFromList, //toast.LENGTH_SHORT).show();
 				goToAim(selectedFromList);
 			}  
 		});
+		
+		listView.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                    int pos, long id) {
+            	
+            	String selectedFromList = (String) (listView.getItemAtPosition(pos));
+            	
+            	deleteDialog(selectedFromList);
+            		
+            	
+                return true;
+            }
+        }); 
+	}
+	
+	 @Override
+	    protected void onResume() {
+	        super.onResume();
+	        //toast.makeText(getApplicationContext(), "resume", //toast.LENGTH_SHORT).show();
+	        //getSharedPrefs();
+	    }
+	 
+	public void populateWithAssets(){
+		try{
+		//if internal storage not available, load objects from assets
+  	  Scanner sc = new Scanner(getAssets().open("projectiles.txt"));
+  	  
+	  	  while(sc.hasNextLine())
+	  	  {
+	  		  if(sc.hasNextDouble()==false) //current token is a string
+	  		  {
+	  			  String key = "";
+	  			  while(sc.hasNextDouble()==false)
+	  			  {
+	  				  key = key + sc.next() + " ";
+	  			  }
+	  			  map.put(key, sc.nextDouble());
+	  		  }
+	  		  else
+	  		  {
+	  			  map.put("null", sc.nextDouble());
+	  		  }
+	  	  }
+	  	  for (String key : map.keySet()) {
+	  		   object_array_list.add(key);
+	  	  }
+	  	  
+	  	  storeSharedPrefs();
+	  	  
+	    } catch (Exception f) {
+	       Log.d("damn", "FAIL");
+	    }
 	}
 
-	public void goToAim(Projectile projectile) {
-		Intent intent = new Intent(this, AimActivity.class);
-		Bundle b = new Bundle();
-		b.putString("objectName", projectile.description());
-		b.putDouble("objectMass", projectile.weight());
-		intent.putExtras(b);
-		startActivity(intent);
-	}
+	public void goToAim(String objectSelected)
+    {
+		storeSharedPrefs();
+    	objectMassSelected = map.get(objectSelected);
+    	Intent intent = new Intent(this, AimActivity.class);
+    	Bundle b = new Bundle();
+    	b.putString("objectName", objectSelected);
+    	b.putDouble("objectMass",objectMassSelected);
+    	intent.putExtras(b);
+        startActivity(intent);
+    }
+	
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+         MenuInflater inflater = getMenuInflater();
+         inflater.inflate(R.menu.addobject, menu);
+          return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+      case R.id.menuItem1:
+    	Intent intent = new Intent(this, AddObjectActivity.class);
+  		startActivity(intent);
+        break;
+      }
+      return true;
+    } 
+    public void storeSharedPrefs(){
+    	SharedPreferences.Editor editor = getSharedPreferences("projectilePrefs", 0).edit();
+
+    	for( Entry<String, Double> entry : map.entrySet() ) 
+    	  editor.putString( entry.getKey(), entry.getValue().toString() );
+    	editor.commit();
+    }
+    public void getSharedPrefs(){
+    	SharedPreferences prefs = getSharedPreferences("projectilePrefs", 0);
+
+    	for( Entry<String, ?> entry : prefs.getAll().entrySet() ) 
+    	  map.put( entry.getKey(), Double.parseDouble(entry.getValue().toString()));
+    	for (String key : map.keySet()) {
+	  		   object_array_list.add(key);
+	  	 }
+    }
+    public void deleteDialog(final String nameOfObject){
+    	
+    	
+    	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+				this);
+    	
+			alertDialogBuilder.setTitle("Delete Object");
+ 
+			// set dialog message
+			alertDialogBuilder
+				.setMessage("Are you sure you want to delete "+nameOfObject+"?")
+				.setCancelable(false)
+				.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						// if this button is clicked, close
+						// current activity
+						map.remove(nameOfObject);	
+		            	SharedPreferences.Editor editor = getSharedPreferences("projectilePrefs", 0).edit();
+		            	editor.remove(nameOfObject);
+		            	editor.commit();
+		            	finish();
+	            		startActivity(getIntent());
+					}
+				  })
+				.setNegativeButton("No",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						// if this button is clicked, just close
+						// the dialog box and do nothing
+						
+						dialog.cancel();
+					}
+				});
+				
+				
+				// create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+ 
+				// show it
+				alertDialog.show();
+				
+    }
+
 }
